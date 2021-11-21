ada8f45e1191ee88629cb5087f3f4f510e1b304e
Amanlamba <19aman9@gmail.com>
2021-11-21 18:39:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,76 @@
+
+// Question 1 code - 
+let n = 5; 
+let string = "";
+
+for(let i = 0; i < n; i++) { 
+  for(let j = 0; j < n; j++) { 
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
+
+// Question 2 code -
+
+
+let n = 5;
+let string = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < i; j++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
+
+// Question 3 code -
+
+let n = 5;
+let string = "";
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n - i; j++) {
+    string += " ";
+  }
+  for (let k = 0; k < 2 * i - 1; k++) {
+    if(i === 1 || i === n) {
+      string += "*";
+    }
+    else {
+      if(k === 0 || k === 2 * i - 2) {
+        string += "*";
+      }
+      else {
+        string += " ";
+      }
+    }
+  }
+  string += "\n";
+}
+console.log(string);
+// Question 4 code - 
+
+let n = 5;
+let string = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = n; j > i; j--) {
+    string += " ";
+  }
+  for (let k = 0; k < i * 2 - 1; k++) {
+    string += "*";
+  }
+  string += "\n";
+}
+for (let i = 1; i <= n - 1; i++) {
+  for (let j = 0; j < i; j++) {
+    string += " ";
+  }
+  for (let k = (n - i) * 2 - 1; k > 0; k--) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
+
+
